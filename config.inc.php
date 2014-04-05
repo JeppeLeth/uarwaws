b092b730a8007ebeab877d9b6265f64769e7f711
JeppeLeth <jeppe.leth@gmail.com>
2014-04-05 19:45:28 +0200

    test

--- config.inc.php
+++ config.inc.php
@@ -4,7 +4,7 @@
  * Local configuration; requires editing.
  */
 
-define('UARWAWS_S3_BUCKET' , 'image.test.resize');
+define('UARWAWS_S3_BUCKET' , 'image-test-resize');
 define('UARWAWS_SDB_DOMAIN' , 'resizedImages');
 define('UARWAWS_SQS_QUEUE' , 'images-to-resize');
 define('UARWAWS_SNS_TOPIC' , 'image-bad-upload');
