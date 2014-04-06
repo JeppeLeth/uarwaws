320bab506417adf8d2e960b56fd436129602928a
JeppeLeth <jeppe.leth@gmail.com>
2014-04-06 11:21:47 +0200

    test

--- pages/status.php
+++ pages/status.php
@@ -5,7 +5,17 @@
  */
  
  try {
-   $aws = Aws\Common\Aws::factory('/config/config.php');
+   $aws = Aws\Common\Aws::factory(array(
+    'includes' => array('_sdk1''),
+    'services' => array(
+        'default_settings' => array(
+            'params' => array(
+                'key'    => 'AKIAI2NIP5SZJAR3H4OA',
+                'secret' => '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',
+                'region' => 'eu-west-1'
+            )
+        )
+    ));
 }
 catch (Exception $e) {
   echo renderMsg('error', array(
