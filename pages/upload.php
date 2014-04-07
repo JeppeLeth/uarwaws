79d6537101a4a14595bc74791ca0240fb97bd96c
JeppeLeth <jeppe.leth@gmail.com>
2014-04-07 19:38:22 +0200

    test

--- pages/upload.php
+++ pages/upload.php
@@ -115,8 +115,9 @@ if (isset($_FILES['image']['tmp_name']) && $_FILES['image']['tmp_name']) {
   }
   
   if ($s3_upload_response->isOK()) {
+	$iso8601_date = get_the_time('c');
     echo renderMsg('success', array(
-      'body' => 'Uploaded image to Amazon S3.' . $s3_upload_response->header['x-aws-requestheaders']['Date'] . '   ' . get_the_time('c'),
+      'body' => 'Uploaded image to Amazon S3.' . $s3_upload_response->header['x-aws-requestheaders']['Date'] . '   ' .  $iso8601_date,
     ));
 	echo "<pre>";
 	print_r($s3_upload_response->header);
