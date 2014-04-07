fb6a3f5204ca263142931d54b9646fdcc91bdabe
JeppeLeth <jeppe.leth@gmail.com>
2014-04-07 22:10:36 +0200

    test

---
+++ process.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Front process controller.
+ */
+define('HIDE_HTML' , TRUE);
+require_once 'config.inc.php';
+require_once 'util.inc.php';
+require_once 'AWSSDKforPHP/sdk.class.php';
+include_once 'pages/process.php';
+?>
+
