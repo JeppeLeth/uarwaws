c65a246d1f98d40cb06f50c1fcd1441099bc8bd7
JeppeLeth <jeppe.leth@gmail.com>
2014-04-07 20:15:01 +0200

    test

--- pages/upload.php
+++ pages/upload.php
@@ -21,10 +21,12 @@ if (isset($_FILES['image']['tmp_name']) && $_FILES['image']['tmp_name']) {
   try {
     $imagick = new Imagick($_FILES['image']['tmp_name']);
     $file_upload_success = TRUE;
-	echo renderMsg('success', array(
-      'heading' => 'Success',
-      'body' => 'Image file upload to server',
-    ));
+	if (!$hide_html) {
+		echo renderMsg('success', array(
+		  'heading' => 'Success',
+		  'body' => 'Image file upload to server',
+		));
+	}
   }
   catch (Exception $e) {
     $file_upload_success = FALSE;
