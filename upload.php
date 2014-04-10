837779d3588da27b8609e8e9ee7b4dd894e8ac2d
JeppeLeth <jeppe.leth@gmail.com>
2014-04-10 09:16:26 +0200

    test

--- upload.php
+++ upload.php
@@ -7,6 +7,7 @@ define('HIDE_HTML' , TRUE);
 require_once 'config.inc.php';
 require_once 'util.inc.php';
 require_once 'AWSSDKforPHP/sdk.class.php';
+header('Access-Control-Allow-Origin: *'); 
 include_once 'pages/upload.php';
 ?>
 
